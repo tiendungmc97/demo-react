b93beddb89730c32e86e9fbd051e9924cebacbd3
DungNT97 <tiendungmc97@gmail.com>
2022-03-23 16:43:03 +0700

    features/Todo:  process action

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./style.scss";
+import classnames from "classnames";
 
 Todo.propTypes = {};
 
-const items = [
-  {
-    name: "Aflreds Futterkiste",
-    status: "complete",
-    action: {
-      complete: true,
-      delete: true,
-    },
-  },
-  {
-    name: "Buy more food",
-    status: "new",
-    action: {
-      complete: true,
-      delete: true,
-    },
-  },
-  {
-    name: "Make more mony",
-    status: "complete",
-    action: {
-      complete: true,
-      delete: false,
-    },
-  },
-];
-
 function Todo(props) {
+  const [valueInput, setValueInput] = useState({
+    inputTask: "",
+  });
+  const inititems = [
+    {
+      name: "Aflreds Futterkiste",
+      status: "new",
+      action: {
+        new: true,
+        depending: true,
+        complete: true,
+        delete: true,
+      },
+    },
+    {
+      name: "aaa",
+      status: "new",
+      action: {
+        new: true,
+        depending: true,
+        complete: true,
+        delete: true,
+      },
+    },
+  ];
+  const [items, setItems] = useState(inititems);
+
+  const handleInput = (e) => {
+    const { value, name } = e.target;
+    setValueInput({
+      ...valueInput,
+      [name]: value,
+    });
+  };
+  const handleSubmit = () => {
+    let task = valueInput.inputTask;
+    let item = [...items];
+    let schema = {
+      name: task,
+      status: "new",
+      action: {
+        new: true,
+        depending: true,
+        complete: true,
+        delete: true,
+      },
+    };
+    item.push(schema);
+    setItems(item);
+  };
+
+  const handleStatus = (index, status) => {
+    let item = [...items];
+    item[index].status = status;
+    setItems(item);
+  };
+
+  console.log("value", items);
   return (
     <div className="todo">
       <div className="todo__title">Todos</div>
@@ -42,9 +74,18 @@ function Todo(props) {
           <input
             className="content__input-todo"
             placeholder="What do you wants to do?"
+            name="inputTask"
+            value={valueInput.inputTask}
+            onChange={handleInput}
           ></input>
+
           <p className="content__note">Enter what you want to procastinate </p>
-          <button className="content_submit btn btn--primary pointer">Submit</button>
+          <button
+            className="content_submit btn btn--primary pointer"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
         </div>
       </div>
       <div className="todo__task task">
@@ -52,19 +93,49 @@ function Todo(props) {
         <div className="task__content content">
           <table className="task__table table">
             <thead>
-              <th>Items </th>
-              <th>Status</th>
-              <th>Action</th>
+              <tr>
+                <th>Items </th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
             </thead>
             <tbody>
-              {items?.map((item) => {
+              {items?.map((item, index) => {
                 return (
-                  <tr>
+                  <tr
+                    key={index}
+                    className={classnames({
+                      new: item.status === "new",
+                      completed: item.status === "completed",
+                      depending: item.status === "depending",
+                      "btn-pre": true,
+                      disabvle: true,
+                    })}
+                  >
                     <td>{item.name}</td>
                     <td>{item.status}</td>
                     <td>
+                      {item.action.new && (
+                        <button
+                          className="content__complete btn btn--primary pointer"
+                          onClick={() => handleStatus(index, "new")}
+                        >
+                          New
+                        </button>
+                      )}
+                      {item.action.depending && (
+                        <button
+                          className="content__complete btn btn--primary pointer"
+                          onClick={() => handleStatus(index, "depending")}
+                        >
+                          Depending
+                        </button>
+                      )}
                       {item.action.complete && (
-                        <button className="content__complete btn btn--primary pointer">
+                        <button
+                          className="content__complete btn btn--primary pointer"
+                          onClick={() => handleStatus(index, "completed")}
+                        >
                           Complete
                         </button>
                       )}
